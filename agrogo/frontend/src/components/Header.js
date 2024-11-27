f801aa0315a3967f38134623c25972293bde7346
Ravindu156 <puresoftlk@gmail.com>
2024-11-27 22:45:59 +0530

    add Tailwind CSS integration and update Header component styles

--- agrogo/frontend/src/components/Header.js
+++ agrogo/frontend/src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
        <div className="flex items-center h-full justify-between">
         <Link to={""}>
           <div className="h-10">
-            <img src={logo} className="h-full" />
+            <img style={{ height: '50px', width: 'auto' }}  src={logo} className="h-full" />
           </div>
         </Link>
 
@@ -18,6 +18,10 @@ function Header() {
             <Link to={"about"}>About</Link>
             <Link to={"contact"}>Contact</Link>
           </nav>
+          <div>
+
+          </div>
+
         
         </div>
 
