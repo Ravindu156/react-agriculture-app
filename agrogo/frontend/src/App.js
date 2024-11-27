0d3ef570670c85a4d0658c8d1b4187c913f6f83e
Ravindu156 <puresoftlk@gmail.com>
2024-11-27 13:19:28 +0530

    sign up page

--- agrogo/frontend/src/App.js
+++ agrogo/frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import About from './components/page/About';
 
 function App() {
   return(
 <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/ecom/about" element={<About />} />
       </Routes>
     </Router>
   )
---
+++ agrogo/frontend/src/components/page/SignUp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import loginSignupImage from '../page/login-animation.gif'
+import { useState } from 'react';
+
+export default function SignUp(){
+
+
+
+
+
+
+
+
+
+    
+}
