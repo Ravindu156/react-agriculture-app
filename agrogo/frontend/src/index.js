2928d27d44864565c3aab59ad273082b7475b4b3
Ravindu156 <puresoftlk@gmail.com>
2024-11-27 12:22:09 +0530

    Comment out routing components in index.js for future reference

--- agrogo/frontend/src/index.js
+++ agrogo/frontend/src/index.js
@@ -8,30 +8,30 @@ import {createBrowserRouter,
   Route,
  RouterProvider} from "react-router-dom";
 
- import Home from './components/page/Home';
+ /*import Home from './components/page/Home';
   import Menu from './components/page/Menu';
   import About from './components/page/About';
   import Contact from './components/page/Contact';
   import Login from './components/page/Login';
   import Newproduct from './components/page/Newproduct';
-
+*/
   
 
 const router=createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
+ /*   <Route path='/' element={<App/>}>
       <Route index element={<Home/>}/>
       <Route path="menu" element={<Menu/>}/>
       <Route path="about" element={<About/>}/>
       <Route path="contact" element={<Contact/>}/>
       <Route path="login" element={<Login/>}/>
-      <Route path="newproduct" element={<Newproduct />} />
+      <Route path="newproduct" element={<Newproduct />} />*/
      
      <React.StrictMode>
     <App />
   </React.StrictMode>
 
-  </Route>
+
 
   )
 
