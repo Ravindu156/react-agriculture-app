3e6960f3657c73d4f7c3876f2205bf3ecc776736
Sanduni0601 <sanduni06nisansala@gmail.com>
2024-11-27 12:26:59 +0530

    Update index.js

--- agrogo/frontend/src/index.js
+++ agrogo/frontend/src/index.js
@@ -3,40 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter,
-  createRoutesFromElements,
-  Route,
- RouterProvider} from "react-router-dom";
+// import {createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//  RouterProvider} from "react-router-dom";
 
- import Home from './components/page/Home';
-  import Menu from './components/page/Menu';
-  import About from './components/page/About';
-  import Contact from './components/page/Contact';
-  import Login from './components/page/Login';
-  import Newproduct from './components/page/Newproduct';
-  import SignUp from './components/page/SignUp';
+//  import Home from './components/page/Home';
+//   import Menu from './components/page/Menu';
+//   import About from './components/page/About';
+//   import Contact from './components/page/Contact';
+//   import Login from './components/page/Login';
+//   import Newproduct from './components/page/Newproduct';
+//   import SignUp from './components/page/SignUp';
   
 
-const router=createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route index element={<Home/>}/>
-      <Route path="menu" element={<Menu/>}/>
-      <Route path="about" element={<About/>}/>
-      <Route path="contact" element={<Contact/>}/>
-      <Route path="login" element={<Login/>}/>
-      <Route path="newproduct" element={<Newproduct />} />
-     <Route path="signup" element={<SignUp/>}/>
-     <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const router=createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<App/>}>
+//       <Route index element={<Home/>}/>
+//       <Route path="menu" element={<Menu/>}/>
+//       <Route path="about" element={<About/>}/>
+//       <Route path="contact" element={<Contact/>}/>
+//       <Route path="login" element={<Login/>}/>
+//       <Route path="newproduct" element={<Newproduct />} />
+//      <Route path="signup" element={<SignUp/>}/>
+//      <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
 
-  </Route>
+//   </Route>
 
-  )
+//   )
 
 
-)
+// )
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
