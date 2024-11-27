c632784786b2b007d836a1c188fa823d0267a927
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2024-11-27 21:17:33 +0530

    update

--- agrogo/backend/ecomcropbackend/index.js
+++ agrogo/backend/ecomcropbackend/index.js
@@ -4,4 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const Stripe = require('stripe')
 
- 
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+
+
