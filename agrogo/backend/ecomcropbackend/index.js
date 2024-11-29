868ca5c08375ee05ad37fce7324112bcbd2ec004
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2024-11-29 15:43:55 +0530

    index updated

--- agrogo/backend/ecomcropbackend/index.js
+++ agrogo/backend/ecomcropbackend/index.js
@@ -8,4 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
+const PORT = process.env.PORT || 8080;
+
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connect to Databse"))
+  .catch((err) => console.log(err));
+
+
 
