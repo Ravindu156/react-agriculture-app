99902d3f3d5e375f76a9209a95ded2b6c549dc8b
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2024-11-29 19:28:12 +0530

    package.json file ecomcropbackend added

--- agrogo/backend/ecomcropbackend/index.js
+++ agrogo/backend/ecomcropbackend/index.js
@@ -1,21 +1,4 @@
-const express= require("express");
-const cors=require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
-const Stripe = require('stripe')
 
-const app = express();
-app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-
-const PORT = process.env.PORT || 8080;
-
-
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("Connect to Databse"))
-  .catch((err) => console.log(err));
 
 
 
