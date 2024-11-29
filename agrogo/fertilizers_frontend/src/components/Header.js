b601c77cebf1e3f29f124787b4beba52ce031f33
Shahla-AAFS <shahla21999@gmail.com>
2024-11-29 20:27:37 +0530

    added icons in header

--- agrogo/fertilizers_frontend/src/components/Header.js
+++ agrogo/fertilizers_frontend/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { FaUserAlt } from "react-icons/fa";
+import {BsCartFill} from "react-icons/bs"
 const Header = () => {
   return (
     <header className='fixed shadow-md w-full h-16 px-2 md:px-4'>
@@ -10,6 +12,18 @@ const Header = () => {
             <img src={logo} className='h-full'/>
         </div>
         </Link>
+        <div className="">
+          <nav>
+
+          </nav>
+          <div className=''>
+            <BsCartFill/>
+          </div>
+          <div className=''>
+            <FaUserAlt/>
+          </div>
+
+        </div>
         </div>
     </header>
     
