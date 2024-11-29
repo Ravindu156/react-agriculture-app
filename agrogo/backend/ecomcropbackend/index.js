5fb42321c7a7ef5739566b5f55071c831080ba50
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2024-11-29 19:57:40 +0530

    PORT created

--- agrogo/backend/ecomcropbackend/index.js
+++ agrogo/backend/ecomcropbackend/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose")
 const app = express()
 app.use(cors())
 
+const PORT = process.env.PORT || 8080
+
 app.get("/",(req,res)=>{
     res.send("Server is running")
 })
