5f3afc5186781139cfd030dbce47e7047bbf6b7d
Ravindu156 <puresoftlk@gmail.com>
2024-11-29 12:59:49 +0530

    add new page components for Home, Menu, Newproduct, Contact, About, and SignUp; update App routes

--- agrogo/frontend/src/App.js
+++ agrogo/frontend/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import About from './components/page/About';
-import SignUp from './components/page/SignUp';
-import Header from './components/Header';
-import Login from './components/page/Login';
-import Newproduct from './components/page/Newproduct';
+import Home from './page/Home';
 
 function App() {
   return(
 <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/ecom/home" element={<Home />} />
         <Route path="/ecom/about" element={<About />} />
         <Route path="/ecom/signup" element={<SignUp />} />
         <Route path="/ecom/header" element={<Header/>}/>
