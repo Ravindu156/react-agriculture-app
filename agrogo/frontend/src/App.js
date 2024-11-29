9a1860d2c20814d0bca755f21dc0efeb6db5b917
Ravindu156 <puresoftlk@gmail.com>
2024-11-29 13:15:48 +0530

    add About, SignUp, Login, Menu components and update App routes; fix Header import in Login page

--- agrogo/frontend/src/App.js
+++ agrogo/frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
+import About from './page/About';
+import SignUp from './page/SignUp';
+import Login from './page/Login';
+import Menu from './page/Menu';
+import Newproduct from './page/Newproduct';
+import HomePage from './components/HomePage';
+import Header from './components/Header';
 
 function App() {
   return(
@@ -12,7 +19,7 @@ function App() {
         <Route path="/ecom/signup" element={<SignUp />} />
         <Route path="/ecom/header" element={<Header/>}/>
         <Route path="/ecom/login" element={<Login/>}/>
-        <Route path="/ecom/newproduct" element={<Newproduct/>}/>
+        <Route path="/ecom/menu" element={<Menu/>}/>
         <Route path="/ecom/newproduct" element={<Newproduct/>}/>
       </Routes>
     </Router>
