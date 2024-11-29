5becc261fb7ea30c80fc3acfc329dbc7437f1064
Shahla-AAFS <shahla21999@gmail.com>
2024-11-29 21:50:29 +0530

    set routes to the pages in index

--- agrogo/fertilizers_frontend/src/index.js
+++ agrogo/fertilizers_frontend/src/index.js
@@ -7,11 +7,18 @@ import { createBrowserRouter,createRoutesFromElements
   ,Route,RouterProvider
 
  } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
  const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
-
+        <Route index element={<Home/>}/>
+        <Route path='menu' element={<Menu/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
     </Route>
   )
  )
