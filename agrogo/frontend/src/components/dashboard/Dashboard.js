8bc049bc0ca1f58e2cd7c7de92c272fc5bb1ec34
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-12-01 11:59:03 +0530

    Create Dashboard.js

---
+++ agrogo/frontend/src/components/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+
+export default function Dashboard(){
+    return(
+        <div>
+        <div className="bg-green-800 dark:bg-gray-900 dark:text-white duration-200 overflow-x-hidden w-full">
+      
+        </div>
+
+            
+        </div>
+    )
+}
