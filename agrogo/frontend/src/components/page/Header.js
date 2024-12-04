65ae7f1ba15363a5cdce0ba1620cbc7ddb1e7e5a
SanchithaSamarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2024-12-04 15:47:42 +0530

    Header creation

--- agrogo/frontend/src/components/page/Header.js
+++ agrogo/frontend/src/components/page/Header.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import logo from "../dashboard/logo.png"
 
-function Header() {
+const Header= () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const userData = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const handleShowMenu = () => {
+    setShowMenu((preve) => !preve);
+  };
+  const handleLogout = () => {
+    dispatch(logoutRedux());
+    toast("Logout successfully");
+  };
+
+  const cartItemNumber = useSelector((state)=>state.product.cartItem)
   return (
-    <div>Header</div>
+    <header className ='fixed shadow-md w-full h-16'>
+      <div className=''>
+          <img src={logo}className="h-full" />
+      </div>
+
+
+    </header>
+   
   )
 }
 
