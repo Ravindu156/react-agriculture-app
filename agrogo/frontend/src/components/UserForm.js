850921dc7b91c9e558bc5ec58b78f913c2157e34
Sanduni0601 <sanduni06nisansala@gmail.com>
2024-12-04 19:18:10 +0530

    Update submit button

--- agrogo/frontend/src/components/UserForm.js
+++ agrogo/frontend/src/components/UserForm.js
@@ -79,6 +79,7 @@ export default function UserForm(){
                 required
               />
             </div>
+            <button type="submit">Submit</button>
         </form>
         </div>
         </div>
