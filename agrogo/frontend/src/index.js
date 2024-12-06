acc1bc9c23d84d176c1f36419ff9b77ae15b776e
Thilina Lakshitha <lakshithathilina74@gmail.com>
2024-12-06 11:12:56 +0530

    Update this fils

--- agrogo/frontend/src/index.js
+++ agrogo/frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
  /*import {createBrowserRouter,
   createRoutesFromElements,
   Route,
@@ -33,6 +34,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>
 );
 reportWebVitals();
