975638f96c2bf971f4f32fa4510cba34298606d1
Thilina Lakshitha <lakshithathilina74@gmail.com>
2024-12-06 08:14:18 +0530

    Update cropapp.js

--- agrogo/frontend/src/components/CropCalendar/CropApp.js
+++ agrogo/frontend/src/components/CropCalendar/CropApp.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Search from './CropComponents/search/search';
+import'./CropApp.css'
+
 
 function App() {
   return(
 <div className='App'>
-
+<Search/>
 </div>
   )
   
