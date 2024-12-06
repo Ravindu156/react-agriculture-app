45c35feeae1dd120eeaa320c80db72bc2d71796f
Sanduni0601 <sanduni06nisansala@gmail.com>
2024-12-06 20:43:09 +0530

    Update User.js

--- agrogo/backend/models/User.js
+++ agrogo/backend/models/User.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    firstname: { type: String, required: true }, 
+    lastname: { type: String, required: true }, 
     name: {type:String, required:true},
     email:{type:String, required:true,uniqye:true},
+    mobile: { type: String, required: true, unique: true },
     region:{type:String, required:true},
     role:{type:String, required:true},
+    nic: { type: String, required: true },
     password:{type:String, required: true}
 });
 
