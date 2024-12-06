f67cd315cd4c07c5b2bd253a808764dab2be9285
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2024-12-06 20:15:25 +0530

    error fixed

--- agrogo/backend/ecomcropbackend/index.js
+++ agrogo/backend/ecomcropbackend/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv").config();
 
 const app = express()
 app.use(cors())
-app.use(express.json({limit "10mb"}))
+app.use(express.json({limit: "10mb"}))
 
 const PORT = process.env.PORT || 8080
 
