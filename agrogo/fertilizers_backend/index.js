74f1d038854f7f47a3c92a949c39d6e8d6dfff02
Shahla-AAFS <shahla21999@gmail.com>
2024-12-11 23:29:13 +0530

    created connection to mongodb

---
+++ agrogo/fertilizers_backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const app = express()
+app.use(cors())
+
+const PORT = process.env.PORT || 8080
+
+//mongoDB connection
+
+mongoose.connect('mongodb://localhost:27017/products') 
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err))
+
+//api
+app.get("/",(req,res)=>{
+    res.send("Server is running")
+})
+
+app.listen(PORT,()=>console.log("server is running at port : " +PORT))
