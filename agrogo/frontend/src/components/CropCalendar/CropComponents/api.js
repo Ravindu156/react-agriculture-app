a18a43118d4e00f4482805074f7aa3b3f7ba5540
Thilina Lakshitha <lakshithathilina74@gmail.com>
2024-12-11 15:38:04 +0530

    Create

---
+++ agrogo/frontend/src/components/CropCalendar/CropComponents/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions ={
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '7aafc54f23mshbff4f725786ddb5p1f9440jsn3fef59bc6060',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
