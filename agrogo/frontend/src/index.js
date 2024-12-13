c28b827246ee0c9466bebd26688f375ac9aafbc8
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-12-13 12:08:40 +0530

    index update

--- agrogo/frontend/src/index.js
+++ agrogo/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
  /*import {createBrowserRouter,
   createRoutesFromElements,
@@ -42,4 +42,4 @@ root.render(
     
   </React.StrictMode>
 );
-reportWebVitals();
+//reportWebVitals();
