0bcafaebe1e17a21b291e8fcf31da9e79d4041e3
Thilina Lakshitha <lakshithathilina74@gmail.com>
2024-12-13 10:42:32 +0530

    Update

--- agrogo/frontend/src/App.js
+++ agrogo/frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Dashboard from './components/dashboard/Dashboard';
 import Register from './components/Register';
-//import CropApp from './components/CropCalendar/CropApp';
+import CropApp from './components/CropCalendar/CropApp';
 import Login from './components/Login'
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Route path = "/register" element = {<Register/>}/>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/cropcalendar" element={<CropApp/>}/> */}
+        {<Route path="/cropcalendar" element={<CropApp/>}/> }
       </Routes>
     </Router>
   )
