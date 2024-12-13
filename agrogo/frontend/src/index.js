3285b041e4dac394aa45dc71204329bb4004866d
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-12-13 12:19:31 +0530

    commit to main

--- agrogo/frontend/src/index.js
+++ agrogo/frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,36 +9,59 @@ import App from './App';
   Route,
  RouterProvider} from "react-router-dom";
 
+<<<<<<< Updated upstream
 import Home from './components/page/Home';
+=======
+ import Home from './components/page/Home';
+>>>>>>> Stashed changes
   import Menu from './components/page/Menu';
   import About from './components/page/About';
   import Contact from './components/page/Contact';
   import Login from './components/page/Login';
   import Newproduct from './components/page/Newproduct';
-*/
+
   
 
 /*const router=createBrowserRouter(
   createRoutesFromElements(
+<<<<<<< Updated upstream
     <Route path='/' element={<App/>}>
+=======
+   <Route path='/' element={<App/>}>
+>>>>>>> Stashed changes
       <Route index element={<Home/>}/>
       <Route path="menu" element={<Menu/>}/>
       <Route path="about" element={<About/>}/>
       <Route path="contact" element={<Contact/>}/>
       <Route path="login" element={<Login/>}/>
       <Route path="newproduct" element={<Newproduct />} />
+<<<<<<< Updated upstream
+=======
+     
+     <React.StrictMode>
+    <App />
+  </React.StrictMode>
+
+
+
+>>>>>>> Stashed changes
   )
 <<<<<<< HEAD
 <<<<<<< HEAD
 
 
 )
+<<<<<<< Updated upstream
 )
 */
 
 =======
 */
 >>>>>>> 3cc51558d8a3073aa2861f985385cacf62ecaa6f
+=======
+
+
+>>>>>>> Stashed changes
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -46,4 +69,20 @@ root.render(
     
   </React.StrictMode>
 );
+<<<<<<< Updated upstream
 //reportWebVitals();
+=======
+
+*/
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+>>>>>>> Stashed changes
