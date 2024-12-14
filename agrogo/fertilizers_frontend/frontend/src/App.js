5478369324423ca7ff455a81929eff0670c421d5
SanchithaSamarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2024-12-14 15:49:25 +0530

    change location

--- agrogo/fertilizers_frontend/frontend/src/App.js
+++ agrogo/fertilizers_frontend/frontend/src/App.js
@@ -1,25 +1,40 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Header from './components/Header';
+//import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-//import Dashboard from './components/dashboard/Dashboard';
+import Dashboard from './components/dashboard/Dashboard';
 import Register from './components/Register';
 import Store from './components/page/Store';
 import Seller from './components/page/Store/StoreCom/Seller';
 
-
 function App() {
   return(
-<Router>
-      <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path = "/register" element = {<Register/>}/>
-      
-      <Route path="/Store" element={<Store />} />
-      <Route path="/Seller" element={<Seller />} />
-      </Routes>
+    <Router>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path = "/register" element = {<Register/>}/>
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/Store" element={<Store />} />
+    <Route path="/Seller" element={<Seller />} />
+    </Routes>
+
+    </Router>
   
-  </Router>
   
-)};
+  
+/*  <div>
+    <Header/>
+    <main className='pt-16'>
+      <Outlet/>
+    </main>
+  </div>*/
+  
+
+
+  );
+}
 
 export default App;
--- agrogo/fertilizers_frontend/frontend/src/index.js
+++ agrogo/fertilizers_frontend/frontend/src/index.js
@@ -1,88 +1,33 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter,createRoutesFromElements
+  ,Route,RouterProvider
 
- /*import {createBrowserRouter,
-  createRoutesFromElements,
-  Route,
- RouterProvider} from "react-router-dom";
+ } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
-<<<<<<< Updated upstream
-import Home from './components/page/Home';
-=======
- import Home from './components/page/Home';
-
-  import Menu from './components/page/Menu';
-  import About from './components/page/About';
-  import Contact from './components/page/Contact';
-  import Login from './components/page/Login';
-  import Newproduct from './components/page/Newproduct';
-
-  
-
-/*const router=createBrowserRouter(
+ const router = createBrowserRouter(
   createRoutesFromElements(
-<<<<<<< Updated upstream
     <Route path='/' element={<App/>}>
-=======
-   <Route path='/' element={<App/>}>
->>>>>>> Stashed changes
-      <Route index element={<Home/>}/>
-      <Route path="menu" element={<Menu/>}/>
-      <Route path="about" element={<About/>}/>
-      <Route path="contact" element={<Contact/>}/>
-      <Route path="login" element={<Login/>}/>
-      <Route path="newproduct" element={<Newproduct />} />
-<<<<<<< Updated upstream
-=======
-     
-     <React.StrictMode>
-    <App />
-  </React.StrictMode>
-
-
-
->>>>>>> Stashed changes
+        <Route index element={<Home/>}/>
+        <Route path='menu' element={<Menu/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
+    </Route>
   )
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-)
-<<<<<<< Updated upstream
-)
-
-
-=======
-
->>>>>>> 3cc51558d8a3073aa2861f985385cacf62ecaa6f
-=======
-
-
->>>>>>> Stashed changes
+ )
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-    
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
-<<<<<<< Updated upstream
-//reportWebVitals();
-=======
 
-*/
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
