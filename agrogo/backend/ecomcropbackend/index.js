242f77cc9cc980701b8a6cf4d3690ff393056772
Ravindu156 <puresoftlk@gmail.com>
2024-12-14 19:30:56 +0530

    update stripe package to version 17.4.0 and add related type definitions

--- agrogo/backend/ecomcropbackend/index.js
+++ agrogo/backend/ecomcropbackend/index.js
@@ -1,71 +1,79 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const Stripe = require('stripe')
 
-const app = express()
-app.use(cors())
-app.use(express.json({limit: "10mb"}))
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 //mongodb connection
-console.log(process.env.MONGODB_URL)
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URL)
-.then(() => console.log("Connect to Databse"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connect to Databse"))
+  .catch((err) => console.log(err));
 
-
-//Schema
-
+//schema
 const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: {
-      type: String,
-      unique: true,
-    },
-    password: String,
-    confirmPassword: String,
-    image: String,
-  });
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  confirmPassword: String,
+  image: String,
+});
 
-  //model
-
-  const userModel = mongoose.model("user",userSchema)
-  
-
+//
+const userModel = mongoose.model("user", userSchema);
 
 //api
-app.get("/",(req,res)=>{
-    res.send("Server is running")
-})
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-// app.post("/signup",(req,res)=>{
-//     console.log(req.body)
-//     const { email } = req.body;
+//sign up
+app.post("/signup", async (req, res) => {
+   console.log(req.body);
+  const { email } = req.body;
 
+  /*userModel.findOne({ email: email }, (err, result) => {
+    // console.log(result);
+    console.log(err);
+    if (result) {
+      res.send({ message: "Email id is already register", alert: false });
+    } else {
+      const data = userModel(req.body);
+      const save = data.save();
+      res.send({ message: "Successfully sign up", alert: true });
+    }
+  });*/
 
-//    const resultData = await userModel.findOne({email : email})
-   
-//     console.log(resultData)
-//     if(!resultData){
-//         const data = userModel(req.body) 
-//                 const save = data.save()
-//                 res.send({message:"Successfully sign up"})
-//     }
-//     else{
-//         res.send({message: "Email id is already registered"})
-//     }
+  const resultData = await  userModel.findOne({email : email})
+     console.log(resultData)
+     if(!resultData){
+         const data = userModel(req.body)
+                 const save = data.save()
+                 res.send({message:"Successfully sign up"})
+     }
+     else{
+         res.send({message: "Email id is already registered"})
+     }
 
+});
 
-// })
-
-app.post("/login",(req,res) => {
-   const {email} =req.body;
-   userModel.findOne({ email : email },(err,result) => {
-    if(result){
+//api login
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  const { email } = req.body;
+  userModel.findOne({ email: email }, (err, result) => {
+    if (result) {
       const dataSend = {
         _id: result._id,
         firstName: result.firstName,
@@ -84,100 +92,88 @@ app.post("/login",(req,res) => {
         message: "Email is not available, please sign up",
         alert: false,
       });
-    
     }
-   })
-})
+  });
+});
 
-
+//product section
 
 const schemaProduct = mongoose.Schema({
-   name:String,
-   category:String,
-   image:String,
-   price:String,
-   description:String,
-
+  name: String,
+  category:String,
+  image: String,
+  price: String,
+  description: String,
 });
 const productModel = mongoose.model("product",schemaProduct)
 
 
-  app.post("/uploadProduct",async(req,res)=>{
-     const data = await productModel(req.body)
-     const datasave = await data.save()
-     res.send({message : "Upload successfully"})
+
+//save product in data 
+//api
+app.post("/uploadProduct",async(req,res)=>{
+    // console.log(req.body)
+    const data = await productModel(req.body)
+    const datasave = await data.save()
+    res.send({message : "Upload successfully"})
 })
 
-
+//
 app.get("/product",async(req,res)=>{
-  const data =await productModel.find({})
+  const data = await productModel.find({})
   res.send(JSON.stringify(data))
 })
+ 
+/*****payment getWay */
+//console.log(process.env.STRIPE_SECRET_KEY)
 
 
+// const stripe  = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-console.log(process.env.STRIPE_SECRET_KEY)
+// app.post("/create-checkout-session",async(req,res)=>{
 
+//      try{
+//       const params = {
+//           submit_type : 'pay',
+//           mode : "payment",
+//           payment_method_types : ['card'],
+//           billing_address_collection : "auto",
+//           shipping_options : [{shipping_rate : "shr_1N0qDnSAq8kJSdzMvlVkJdua"}],
 
+//           line_items : req.body.map((item)=>{
+//             return{
+//               price_data : {
+//                 currency : "inr",
+//                 product_data : {
+//                   name : item.name,
+//                   // images : [item.image]
+//                 },
+//                 unit_amount : item.price * 100,
+//               },
+//               adjustable_quantity : {
+//                 enabled : true,
+//                 minimum : 1,
+//               },
+//               quantity : item.qty
+//             }
+//           }),
 
+//           success_url : `${process.env.FRONTEND_URL}/success`,
+//           cancel_url : `${process.env.FRONTEND_URL}/cancel`,
 
-//const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+//       }
 
-
-app.post("/create-checkout-session",async(req,res)=>{
-
-  
- try{
-  const params = {
-       submit_type : 'pay',
-       mode : "payment",
-       payment_method_types : ['card'],
-       billing_address_collection : "auto",
-       shipping_options : [{shipping_rate : "shr_1N0qDnSAq8kJSdzMvlVkJdua"}],
-
-
-       
-
-        line_items : req.body.map((item)=>{
-          
-          return{
-            price_data : {
-              currency : "inr",
-              product_data : {
-                name : item.name,
-              },
-              
-              unit_amount : item.price * 100,
-
-            },
-
-            adjustable_quantity : {
-              enabled : true,
-              minimum :1,
-            },
-
-
-            quantity : item.qty
-          }
-        }),
-
-        success_url : `${process.env.FRONTEND_URL}/success`,
-        cancel_url : `${process.env.FRONTEND_URL}/cancel`,
-
-      }
       
-      
+//       const session = await stripe.checkout.sessions.create(params)
+//       // console.log(session)
+//       res.status(200).json(session.id)
+//      }
+//      catch (err){
+//         res.status(err.statusCode || 500).json(err.message)
+//      }
 
-      const session = await stripe.checkout.sessions.create(params)
-       res.status(200).json(session.id)
-    }
-
-    catch(err){
-      res.status(err.statuscode || 500).json(err.message)
-    }
-})
-  
-
-app.listen(PORT,()=>console.log("Server is running at port :" + PORT))
+// })
 
 
+//server is ruuning
+app.listen(PORT, () => console.log("server is running at port : " + PORT));
