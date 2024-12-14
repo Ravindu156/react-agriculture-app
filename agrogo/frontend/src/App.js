86df401619ea1ffe9345b099dc280e9730ddeff6
Sanchitha Samarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2024-12-14 13:31:44 +0530

    Revert "Feature/fertilizers/ecommerce"

--- agrogo/frontend/src/App.js
+++ agrogo/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-
+import Dashboard from './components/dashboard/Dashboard';
 import Register from './components/Register';
 import Store from './components/page/Store';
 import Seller from './components/page/Store/StoreCom/Seller';
@@ -13,7 +13,7 @@ function App() {
       <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path = "/register" element = {<Register/>}/>
-     
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/Store" element={<Store />} />
       <Route path="/Seller" element={<Seller />} />
       </Routes>
