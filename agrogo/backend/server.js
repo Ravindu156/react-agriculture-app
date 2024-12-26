3f4f316cebcda9d1b76c94efb4791b4fd4017f60
Sanduni0601 <sanduni06nisansala@gmail.com>
2024-12-26 16:32:00 +0530

    set chatbot backend

--- agrogo/backend/server.js
+++ agrogo/backend/server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const userRoutes = require('./routes/users');
-
+require('dotenv').config();
+const chatRoutes = require('./routes/ChatBot')
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 
@@ -17,5 +18,6 @@ mongoose.connect("mongodb+srv://sanduni06nisansala:GXxRkwZRrQmkzQB2@agrogo.rbxzr
 
 
     app.use('/api/users', userRoutes);
+    app.use('/api/chat',chatRoutes)
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
