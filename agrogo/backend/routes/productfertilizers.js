b9c0aabe7f3a60392a9a2f8c21aa8b3d6b8157e9
Shahla-AAFS <shahla21999@gmail.com>
2025-01-01 23:24:59 +0530

    put verifyToken to for security

--- agrogo/backend/routes/productfertilizers.js
+++ agrogo/backend/routes/productfertilizers.js
@@ -2,12 +2,12 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const productfertilizerModel = require('../models/ProductFertilizer')
-
+const verifyToken = require("./auth");
 const router = express.Router();
 
 //save product in data
 //api
-router.post("/uploadProduct",async(req,res)=>{
+router.post("/uploadProduct",verifyToken,async(req,res)=>{
     console.log(req.body)
     const data = await productfertilizerModel(req.body)
     const datasave = await data.save()
@@ -16,7 +16,7 @@ router.post("/uploadProduct",async(req,res)=>{
 
 
 //
-router.get("/product",async(req,res)=>{
+router.get("/product",verifyToken,async(req,res)=>{
     const data = await productfertilizerModel.find({})
     res.send(JSON.stringify(data))
   })
