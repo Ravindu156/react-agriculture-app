f47f662706e5383ce4b352d0771c89e13b94b24a
Shahla-AAFS <shahla21999@gmail.com>
2025-01-01 23:11:19 +0530

    put verifytoken

--- agrogo/fertilizers_backend/index.js
+++ agrogo/fertilizers_backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+const verifyToken = require("./auth");
 
 const app = express()
 app.use(cors())
