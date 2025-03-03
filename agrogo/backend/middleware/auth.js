24b3ba51a7a93e38011a961815291b526a617c21
Shahla-AAFS <shahla21999@gmail.com>
2025-03-03 14:43:49 +0530

    set api for profile page functions

--- agrogo/backend/middleware/auth.js
+++ agrogo/backend/middleware/auth.js
@@ -1,14 +1,27 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   const token = req.header('Authorization');
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
 
   try {
-    const decoded = jwt.verify(token, 'vau@group14');
-    req.user = decoded;
+    let pureToken = token;
+    if (token.startsWith('Bearer ')) {
+      pureToken = token.split(' ')[1];
+    }
+    const decoded = jwt.verify(pureToken, 'vau@group14');
+    
+
+    //  check whether the the user in DB
+    const user = await User.findById(decoded.userId).select('-password');
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
+    }
+
+    req.user = user;
     next();
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
