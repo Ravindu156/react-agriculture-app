6952b690bda4b8ae18694167502a326d1c419b45
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-03-15 18:52:29 +0530

    Change some styles

--- agrogo/frontend/src/page/About.js
+++ agrogo/frontend/src/page/About.js
@@ -106,15 +106,15 @@ const ReviewPage = () => {
                     />
                   </div>
 
-                  {/* Star Rating */}
-                  <div className="flex flex-col items-center my-6">
+                  {/* Star Rating - Modified to align left */}
+                  <div className="flex flex-col items-start my-6">
                     <div className="flex items-center mb-2">
                       <svg className="w-5 h-5 mr-2 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                       </svg>
                       <span className="text-white">Your Rating</span>
                     </div>
-                    <div className="flex justify-center space-x-3">
+                    <div className="flex justify-start space-x-3">
                       {[1, 2, 3, 4, 5].map((star) => (
                         <span
                           key={star}
