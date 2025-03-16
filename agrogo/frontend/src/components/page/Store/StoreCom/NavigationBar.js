c3b0e1ae678333f674d44bb625d4644f1d26c6a6
Ravindu Hirusanka <puresoftlk@gmail.com>
2025-03-16 20:15:32 +0530

    feat:set Admin visible only to Executive Officers

--- agrogo/frontend/src/components/page/Store/StoreCom/NavigationBar.js
+++ agrogo/frontend/src/components/page/Store/StoreCom/NavigationBar.js
@@ -3,25 +3,32 @@ import '../StoreAssets/StoreNav.css';
 import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
+  // Retrieve and parse the stored user data
+  const userData = JSON.parse(localStorage.getItem("user"));  
+  const userRole = userData?.role || ""; // Handle null/undefined case
+
   return (
     <nav className="nav-bar">
-    <ul>
-      
-      <li><Link to="/inorganic" className="ex-button">EXCHANGE</Link></li>
-      <li>
+      <ul>
+        <li>
+          <Link to="/inorganic" className="ex-button">EXCHANGE</Link>
+        </li>
+        <li>
           <Link to="/Seller" className="sell-button">ADD ORDERS</Link>
-      </li>
-      <li>
+        </li>
+        <li>
           <Link to="/Buy" className="buy-button">FILL ORDERS</Link>
-      </li>
-      <li>
-          <Link to="/Admin" className="admin-button">ADMIN</Link>
-      </li>
-      <li>
-          <Link to="/#about-us" className="home-button">HOME</Link>
-      </li>
-    </ul>
-  </nav>
+        </li>
+        <li>
+          {/* Show Admin link only if the role is NOT "user" */}
+          {userRole !== "user" && <Link to="/Admin" className="admin-button">ADMIN</Link>}
+        </li>
+        <li>
+          {/* Use <a> for hash navigation */}
+          <a href="/#about-us" className="home-button">HOME</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
