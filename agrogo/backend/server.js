4f0c8605460600b20d5af6d19ed4cb0d51563fb2
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-03-22 14:11:01 +0530

    correct server.js

--- agrogo/backend/server.js
+++ agrogo/backend/server.js
@@ -54,8 +54,5 @@ mongoose
 
 // Start Server
 const PORT = 5000;
- console.log('Email credentials:', {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD ? 'Password exists' : 'Password missing'
-      });
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
